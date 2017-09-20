c282fff6fed7ddf877886b5c4b5ee07ecba3bb4a
gomesuit <you@example.com>
2017-09-21 02:55:55 +0900

    save

---
+++ README.md
@@ -0,0 +1,7 @@
+# readme
+
+- https://github.com/pimoroni/scroll-phat-hd
+
+```sh
+curl https://get.pimoroni.com/scrollphathd | bash
+```
---
+++ hello-world.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import time
+import signal
+
+import scrollphathd
+from scrollphathd.fonts import font5x7
+
+print("""
+Scroll pHAT HD: Hello World
+
+Scrolls "Hello World" across the screen
+in a 5x7 pixel large font.
+
+Press Ctrl+C to exit!
+
+""")
+
+#Uncomment to rotate the text
+scrollphathd.rotate(180)
+
+#Set a more eye-friendly default brightness
+scrollphathd.set_brightness(0.5)
+
+scrollphathd.write_string("Hello World! ", x=0, y=0, font=font5x7, brightness=0.5)
+
+while True:
+    scrollphathd.show()
+    scrollphathd.scroll()
+    time.sleep(0.05)
+
